391936e46ed833bb0097aa2a8207747404fffafd
tim <timhahn333@gmail.com>
2020-01-16 18:11:20 -0800

    week 2 exercises

--- chapter-07-exercises/exercise-2/exercise.R
+++ chapter-07-exercises/exercise-2/exercise.R
@@ -2,24 +2,25 @@
 
 # Create a vector `first_ten` that has the values 10 through 20 in it (using 
 # the : operator)
-
+first_ten <- c(10:20)
 
 # Create a vector `next_ten` that has the values 21 through 30 in it (using the 
 # seq() function)
-
+next_ten <- seq(21, 30)
 
 # Create a vector `all_numbers` by combining the previous two vectors
-
+all_numbers <- c(first_ten, next_ten)
 
 # Create a variable `eleventh` that contains the 11th element in `all_numbers`
-
+eleventh <- all_numbers[11]
 
 # Create a vector `some_numbers` that contains the 2nd through the 5th elements 
 # of `all_numbers`
-
+some_numbers <- all_numbers[2:5]
 
 
 # Create a vector `even` that holds the even numbers from 1 to 100
+seq(2, 100, 2)
 
 
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
